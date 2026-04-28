b735f63d0f37aaf0d75553ff127025294a292aff
aagust11 <132393883+aagust11@users.noreply.github.com>
2026-04-28 08:12:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+
+  base: '/gestor_tac2/',
+
   const env = loadEnv(mode, '.', '');
   return {
     base: './',
